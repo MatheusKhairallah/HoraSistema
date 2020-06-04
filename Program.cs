a900394c60c82685da81d282c3a2381187f817c9
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:48:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            DateTime horario=DateTime.Now;
+            Console.WriteLine($"Olá usuário, são agora {horario.TimeOfDay} do sistema de seu computador");
+        }
+    }
+}
